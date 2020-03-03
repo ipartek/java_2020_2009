4acb3bb0495ec9f9b9b8eaff43e6d71e1999d128
anderuraga <ander.uraga@gmail.com>
2020-03-03 08:46:03 +0100

    resuelto ejercicio y nuevo

---
+++ src/com/ipartek/formacion/JugandoConString.java
@@ -0,0 +1,25 @@
+package com.ipartek.formacion;
+
+public class JugandoConString {
+
+	public static void main(String[] args) {
+
+		String nombre = "Jose Antonio Friolan de todos los Santos";
+
+		// longitud
+
+		// numero vocales
+
+		// numero consonantes
+
+		// numero de palabras
+
+		// buscar posicion de una letra concreta
+
+		// limpiar espacios en blanco
+
+		// sustituir una letra por otra
+
+	}
+
+}
--- src/com/ipartek/formacion/bucles/PaisesCoronaVirus.java
+++ src/com/ipartek/formacion/bucles/PaisesCoronaVirus.java
@@ -1,30 +1,68 @@
 package com.ipartek.formacion.bucles;
 
+import java.util.Scanner;
+
 public class PaisesCoronaVirus {
 
 	public static void main(String[] args) {
 
-		final String[] PAISES = { "Noruega", "Alemania", "Republica Checa", "Italia" };
+		final String[] PAISES = { "Noruega", "Alemania", "Republica Txeka", "Italia" };
 		boolean[] paisesInfectados = { false, false, false, false };
 		int[] numeroInfectados = { 0, 0, 0, 0 };
+		int total = 0;
+		Scanner sc = new Scanner(System.in);
 
 		// recorrer paises y preguntar si existe algun caso de corona virus
 		for (int i = 0; i < PAISES.length; i++) {
 
-		}
+			System.out.println("¿ Tiene algun caso de CV " + PAISES[i] + " ?   Si o No");
+			String pregunta1 = sc.nextLine();
 
-		// en caso afirmativo, preguntar numero de infectados
+			if ("si".equalsIgnoreCase(pregunta1)) {
 
-		paisesInfectados[1] = true;
-		numeroInfectados[1] = 5;
+				System.out.println("¿Cuantos casos?");
+				int pregunta2 = Integer.parseInt(sc.nextLine());
+				total += pregunta2;
+
+				// actualizar arrays
+				paisesInfectados[i] = true;
+				numeroInfectados[i] = pregunta2;
+
+			}
+
+		} // for de las preguntas
 
 		// finalmente
 
 		// 1: mostrar paises libres de corona virus
+		System.out.println("Paises NO Infectados:");
+		System.out.println("------------------------------------");
+		for (int i = 0; i < PAISES.length; i++) {
+
+			if (!paisesInfectados[i]) {
+				System.out.println(PAISES[i]);
+			}
+
+		}
 
 		// 2: mostrar paises infectados junto con su numero de casos
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Paises Infectados:");
+		System.out.println("------------------------------------");
+		for (int i = 0; i < PAISES.length; i++) {
+
+			if (paisesInfectados[i]) {
+				System.out.println(PAISES[i] + " " + numeroInfectados[i]);
+			}
+
+		} // for
 
 		// 3: total numero de personas infectadas
+
+		System.out.println("TOTAL Infectados: " + total);
+
+		sc.close();
 
 	}
 }
