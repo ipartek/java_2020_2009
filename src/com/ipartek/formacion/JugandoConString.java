190d8f3fbfa09651331059f09dd8c9ae731a4d1a
anderuraga <ander.uraga@gmail.com>
2020-03-05 09:07:31 +0100

    ejercicios planteados para aprender Arrays y Character

--- src/com/ipartek/formacion/JugandoConString.java
+++ src/com/ipartek/formacion/JugandoConString.java
@@ -4,21 +4,95 @@ public class JugandoConString {
 
 	public static void main(String[] args) {
 
-		String nombre = "Jose Antonio Friolan de todos los Santos";
+		// ¿ Que valor tiene por defecto un String si no lo inicializamos ?
+		String sinInicializar;
+
+		String nombre = " AaAnN 5 ";
 
 		// longitud
+		System.out.println("Longitud del nombre " + nombre.length());
 
 		// numero vocales
+		int numeroVocales = 0;
+		int numeroMayusculas = 0;
+		int numeroMinusculas = 0;
+		int numeroConsonantes = 0;
 
-		// numero consonantes
+		// TODO contar numeros y espacios en blanco
+
+		char caracter;
+		for (int i = 0; i < nombre.length(); i++) {
+
+			caracter = nombre.charAt(i);
+			System.out.println("En la posicion " + i + " tenemos caracter " + caracter);
+
+			// contar mayusculas y minusculas
+			if (Character.isLowerCase(caracter)) {
+				numeroMinusculas++;
+			}
+			if (Character.isUpperCase(caracter)) {
+				numeroMayusculas++;
+			}
+
+			// Contar (Vocales o Consonates) Si => es Letra
+
+			if (Character.isLetter(caracter)) {
+
+				caracter = Character.toLowerCase(caracter);
+				switch (caracter) {
+				case 'a':
+				case 'e':
+				case 'i':
+				case 'o':
+				case 'u':
+					numeroVocales++;
+					break;
+
+				default:
+					numeroConsonantes++;
+					break;
+				}// switch
+
+			} // if isLetter
+
+		} // for
+
+		System.out.println("Numero Mayusculas " + numeroMayusculas);
+		System.out.println("Numero Minusculas " + numeroMinusculas);
+		System.out.println("Numero Vocales " + numeroVocales);
+		System.out.println("Numero Consonantes " + numeroConsonantes);
 
 		// numero de palabras
 
+		String palabras = "hola mi nombre es Ander";
+		String[] arrayPalabras = palabras.split(" ");
+
+		System.out.println("Tenemos " + arrayPalabras.length + " palabras en la frase");
+
+		String lineaCVS = "jose;froilan;16;35.000";
+		// TODO mostrar nombre y apellido, la edad y el sueldo anual
+
 		// buscar posicion de una letra concreta
+		String buscarLetra = "Abracadrabra";
+		System.out.println("La primera b esta en la poscion " + buscarLetra.indexOf('b'));
+		System.out.println("La primera a esta en la poscion " + buscarLetra.indexOf('a'));
+		System.out.println("¿Que pasa si no encuentra? " + buscarLetra.indexOf('z'));
+		// TODO buscar primera 'd'
+		// TODO buscar posicion ultima 'a'
 
 		// limpiar espacios en blanco
+		String datosIntroducidosUsuario = "    minombre       es Manolo    ";
+		String limpiarEspacios = datosIntroducidosUsuario.trim();
+		limpiarEspacios = limpiarEspacios.replace("  ", "");
+		System.out.println(limpiarEspacios);
 
-		// sustituir una letra por otra
+		// TODO cambiar numeros por letras y ademas pasar todo a MAYSUCULAS
+		String textoCodificado = " h0l4 m1 n0mb3 3s m4n0l0 ";
+
+		// TODO sumar todos los numeros de este String
+		String dni = "12345678";
+
+		// TODO buscar por interenet como se calcula la letra
 
 	}
 
