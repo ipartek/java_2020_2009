1daa03757fb3a3a0fc122221835b102d293e24ef
anderuraga <ander.uraga@gmail.com>
2020-03-06 13:02:04 +0100

    ejemplo1 para excepciones

---
+++ src/com/ipartek/formacion/excepciones/Ejercicio1.java
@@ -0,0 +1,41 @@
+package com.ipartek.formacion.excepciones;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+
+		int[] aNumeros = new int[3];
+		int suma = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Te voy a pedir " + aNumeros.length + " numeros y luego los sumare");
+
+		for (int i = 0; i < aNumeros.length; i++) {
+
+			boolean repetir = true;
+			do {
+				System.out.println("Dime el numero " + (i + 1));
+				String linea = sc.nextLine();
+
+				try {
+					int numero = Integer.parseInt(linea);
+					suma += numero;
+					repetir = false;
+				} catch (Exception e) {
+					System.out.println("Lo siento pero eso no es un numero");
+					// descomentar esta linea para ver el fallo
+					// e.printStackTrace();
+
+				}
+
+			} while (repetir);
+
+		} // for
+
+		System.out.println("La suma es " + suma);
+		sc.close();
+
+	}// main
+
+}// clase
