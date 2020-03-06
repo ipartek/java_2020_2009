9581e8d9b4b3f394e1a260bb85f74e9412c9497d
anderuraga <ander.uraga@gmail.com>
2020-03-06 08:31:45 +0100

    planteaedo ejericios

---
+++ src/prueba/UF2404EjercicioA.java
@@ -0,0 +1,42 @@
+package prueba;
+
+/**
+ * Realizar un programa en java que realice la siguiente función: Crear un array
+ * de 5 notas que almacenara en formato double. Una vez creado dicho array se
+ * rellenaran dichos elementos leyéndolo desde el teclado. Una vez creado el
+ * contenido, se calculara la media de las notas así como se mostraran las notas
+ * más altas y más bajas de dicho array
+ * 
+ * @author ur00
+ *
+ */
+public class UF2404EjercicioA {
+
+	public static void main(String[] args) {
+		
+		// declarar variables :  arrayNotas, media, maxima, minima
+		// Abrir Scanner
+		
+		// hacer un for con la longitud del array
+		
+				// preguntar por pantalla la nota
+		        // guardar en el array la nota
+		
+				// sumar la nota en la variable media
+		
+			
+				// comprobar si es la nota maxima para guardarla en la variable 'maxima'
+		        // lo mismo con la minima
+		
+		
+		// end for	
+		
+		
+		// usar la variable media para conseguir la 'media' de notas
+		
+		
+		// Cerrar Scanner
+
+	}
+
+}
---
+++ src/prueba/UF2404EjercicioB.java
@@ -0,0 +1,39 @@
+package prueba;
+
+/**
+ * Crear un proyecto en modo consola que nos muestre un menú de 3 opciones:
+ * <ol>
+ * <li>Calcular la letra del dni</li>
+ * <li>Calcular medida de cm a pulgadas</li>
+ * <li>Salir</li>
+ * </ol>
+ * 
+ * 
+ * Hacer que dicho menú se ejecute en bucle hasta que se pulse salir, y si el
+ * usuario escoge cada una de las funciones, que permita introducir los datos
+ * correspondientes por teclado para poder dar el resultado
+ * 
+ * @author ur00
+ *
+ */
+public class UF2404EjercicioB {
+
+	public static void main(String[] args) {
+		
+		// Abrir scanner
+		
+		// do
+		
+			// pintar menu
+			
+			// pedir una opcion al usuario por pantalla
+			// switch case
+		
+		// while( continuar )
+		
+
+		// cerrrar scanner
+		
+	}
+
+}
